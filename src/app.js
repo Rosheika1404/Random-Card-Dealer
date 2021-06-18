8ba12f63c6d0bc4924c71750fc746cc1def023e6
Sheika1404 <rosheika1404@gmail.com>
2021-06-18 02:33:58 -0400

    Second Commit
    - working numbers
    - working suites
    need to add color and need to properly position items

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload=function(){
+
+  //numbers
+document.querySelector("#top-suite-number").innerHTML= generateDeckNumber();
+document.querySelector("#bottom-suite-number").innerHTML= document.querySelector("#top-suite-number").innerHTML;
+document.querySelector("#number").innerHTML= document.querySelector("#top-suite-number").innerHTML;
+
+  //suites
+document.querySelector("#top-suite").innerHTML=generateDeckSuite();
+document.querySelector("#bottom-suite").innerHTML= document.querySelector("#top-suite").innerHTML;
+
+
 };
+
+
+
+// function to generate random number
+let getRandomNumber = (max) => Math.floor(Math.random() * max);
+let deck=["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+let suite=["♥","♦","♠","♣"]
+
+//generate Deck Number
+generateDeckNumber = () =>
+`${deck[getRandomNumber(deck.length)]}`;
+
+//Generate suite
+generateDeckSuite = () =>
+`${suite[getRandomNumber(suite.length)]}`;
+
+console.log(generateDeckNumber(),generateDeckSuite());
+
