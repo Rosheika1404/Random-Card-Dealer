b39bb87d3c7a7edf66d74fb3972fc65b485f79ad
Sheika1404 <rosheika1404@gmail.com>
2021-06-19 22:23:42 -0400

    third commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-window.onload=function(){
+const { get } = require("jquery");
+
+window.onload = function () {
 
   //numbers
-document.querySelector("#top-suite-number").innerHTML= generateDeckNumber();
-document.querySelector("#bottom-suite-number").innerHTML= document.querySelector("#top-suite-number").innerHTML;
-document.querySelector("#number").innerHTML= document.querySelector("#top-suite-number").innerHTML;
+  document.querySelector("#top-suite-number").innerHTML = generateDeckNumber();
+  document.querySelector("#bottom-suite-number").innerHTML = document.querySelector("#top-suite-number").innerHTML;
+  document.querySelector("#number").innerHTML = document.querySelector("#top-suite-number").innerHTML;
 
   //suites
-document.querySelector("#top-suite").innerHTML=generateDeckSuite();
-document.querySelector("#bottom-suite").innerHTML= document.querySelector("#top-suite").innerHTML;
-
+  document.querySelector("#top-suite").innerHTML=(generateDeckSuite());
+  document.querySelector("#bottom-suite").innerHTML = document.querySelector("#top-suite").innerHTML;
 
 };
 
@@ -16,16 +17,25 @@ document.querySelector("#bottom-suite").innerHTML= document.querySelector("#top-
 
 // function to generate random number
 let getRandomNumber = (max) => Math.floor(Math.random() * max);
-let deck=["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
-let suite=["♥","♦","♠","♣"]
+const deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+const suite = ["♥", "♦", "♠", "♣"]
+
 
 //generate Deck Number
 generateDeckNumber = () =>
-`${deck[getRandomNumber(deck.length)]}`;
+  `${deck[getRandomNumber(deck.length)]}`;
 
 //Generate suite
-generateDeckSuite = () =>
-`${suite[getRandomNumber(suite.length)]}`;
+generateDeckSuite = () => {
+  let suite = ["♥", "♦", "♠", "♣"]
+  return suite[getRandomNumber(suite.length)]
+  
+};
 
-console.log(generateDeckNumber(),generateDeckSuite());
-
+get color(){
+  return this.suite ==="♣" || this.suite ==="♠" ? "black" : "red"
+}
+function getHTML(){
+  
+}
+console.log(generateDeckNumber(), generateDeckSuite());
