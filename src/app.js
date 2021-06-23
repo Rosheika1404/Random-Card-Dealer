e0b54b4d496d21a7f0ff2608d1a5c2120e96935a
Sheika1404 <rosheika1404@gmail.com>
2021-06-23 04:59:24 +0000

    still trying

--- src/app.js
+++ src/app.js
@@ -1,38 +1,33 @@
-// window.onload=()=>
-//     document.querySelector(#card).classList.add(generateSuite());
-//     document.querySelector(#card).innerHTML = generateDeck();
+//generate Deck values
+let generateDeck = () => {
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
 
-//   ///generate Deck values
-//   let generateDeck = () => {
-//     const values = [
-//       "A",
-//       "2",
-//       "3",
-//       "4",
-//       "5",
-//       "6",
-//       "7",
-//       "8",
-//       "9",
-//       "10",
-//       "J",
-//       "Q",
-//       "K"
-//     ];
-//     let getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
+  return values[getRandomNumber(values.length)];
+};
+generateDeck();
 
-//     return values[getRandomNumber(values.length)];
-//   };
+// Generate deck suites
+let generateSuite = () => {
+  const suite = ["♥", "♦", "♠", "♣"];
+  let getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
 
-//   generateDeck();
+  return suite[getRandomNumber(suite.length)];
+};
+generateSuite();
 
-//   // Generate deck suites
-//   let generateSuite = () => {
-//     const suite = ["hearts", "diamonds", "clubs", "spades"];
-//     let getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
-
-//     return suite[getRandomNumber(suite.length)];
-//   };
-//   generateSuite();
-
-// //   const suite = ["♥", "♦", "♠", "♣"]
+//   const suite = ["♥", "♦", "♠", "♣"]
