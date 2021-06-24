a26561db57c3cae37f6c30bbf408900e82a94625
Sheika1404 <rosheika1404@gmail.com>
2021-06-24 19:19:29 -0400

    tried

--- src/app.js
+++ src/app.js
@@ -1,48 +1,43 @@
-// window.onload = function() {
-//   //   document.querySelectorAll(".suite").innerHTML = generateSuite();
-//   document.querySelectorAll(".value, middle").innerHTML = generateDeck();
-// };
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-// //generate Deck values
-// function generateDeck() {
-//   const values = [
-//     "A",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "10",
-//     "J",
-//     "Q",
-//     "K"
-//   ];
+const getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
 
-//   let getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
-//   let value = values[getRandomNumber(values.length)];
-//   return value;
+const generateCardValues = () =>
+    `${values[getRandomNumber(values.length)]}`;
+// console.log(generateCardValues());
+
+let value = generateCardValues()
+
+
+
+const cardValue = () => {
+    document.querySelector(".middle").innerHTML = value;
+    document.querySelector(".value").innerHTML = document.querySelector(".middle").innerHTML
+    document.querySelector(".value2").innerHTML = document.querySelector(".middle").innerHTML
+
+    // document.querySelector(".suite").innerHTML = suit;
+    // document.querySelector(".suite2").innerHTML = document.querySelector(".suite").innerHTML
+
+};
+
+// const suite = ["♥", "♦", "♠", "♣"];
+// const getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
+
+// const generateCardSuites = () =>
+//     `${suite[getRandomNumber(suite.length)]}`;
+
+// let suit = generateCardSuites()
+
+
+// {
+//     if (suit === "♥" || suit === "♦") {
+//         // console.log("color red");
+//         return style.color = "red"
+//     } else {
+//         // console.log(suit)
+//         return style.color = "black"
+//     }
 // }
 
-// // Generate deck suites
-// function generateSuite() {
-//   const suite = ["♥", "♦", "♠", "♣"];
-//   let getRandomNumber = max => Math.floor(Math.random() * max); // function to generate random number
-//   let suit = suite[getRandomNumber(suite.length)];
-//   return suit
 
-//   function suiteColor(suit) {
-//     let color = red;
-//     if (suit ==="♥" || suit === "♦") {
-//       document.querySelectorAll(".suite").style.color = "red";
-//     }
-//   }
 
-//   let suiteValue =
-//   suiteColor(suiteValue)
-//   document.querySelectorAll('').innerHTML=suiteColor;
-// };
-
-// console.log(generateDeck(), generateSuite());
